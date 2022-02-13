cd4e1a1f5df7c2529eda28b4a02e042b5e33fef7
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-02-13 12:29:22 -0500

    status code bug fix

--- tests/device_test.py
+++ tests/device_test.py
@@ -15,65 +15,69 @@ from devices import deviceInfo, read_data, status_code
 #         "a1b2c3"
 #         ]
 
-status = status_code()
-
 
 # Test 1 - device keys
 def test_keys_auth():
+    s1 = status_code()
     t1 = deviceInfo("thermometer", "thermo1", "C")
-    read_data("12345", t1, status)
+    read_data("12345", t1, s1)
     expected = ['Invalid key']
     # captured = capsys.readouterr()
 
-    assert expected == status.error
+    assert expected == s1.error
 
 
 # Test 2 - Wrong type of device
 def test_device_type():
+    s2 = status_code()
     t2 = deviceInfo("bmi_monitor", "BMI_1")
-    read_data("a1b2c3", t2, status)
+    read_data("a1b2c3", t2, s2)
     expected = ['Invalid device type']
     # captured = capsys.readouterr()
 
-    assert expected == status.error
+    assert expected == s2.error
 
 
-# Test 4 - Wrong measurements for device type
+# Test 3 - Wrong measurements range for thermometer
 def test_thermo_C():
+    s3 = status_code()
     t3 = deviceInfo("thermometer", "thermoC", "C")
     t3.set_measurements(50.3)
-    read_data("a1b2c3", t3, status)
+    read_data("a1b2c3", t3, s3)
     expected = ['Invalid measurements']
     # captured = capsys.readouterr()
 
-    assert expected == status.error
+    assert expected == s3.error
 
 
 def test_thermo_F():
+    s4 = status_code()
     t4 = deviceInfo("thermometer", "thermoF", "F")
     t4.set_measurements(200)
-    read_data("a1b2c3", t4, status)
+    read_data("a1b2c3", t4, s4)
     expected = ['Invalid measurements']
     # captured = capsys.readouterr()
     
-    assert expected == status.error
+    assert expected == s4.error
 
 
+# Test 4 
 def test_multiple_errors():
+    s5 = status_code()
     t5 = deviceInfo("bmi_monitor", "BMI_1")
-    read_data("a1b2c3", t5, status)
-    expected = ['Invalid device type']
+    read_data("a1b2c3123456", t5, s5)
+    expected = ['Invalid key', 'Invalid device type']
     # captured = capsys.readouterr()
 
-    assert expected == status.error
+    assert expected == s5.error
 
-
-# Test - wrong scales
+# Test - wrong scales for device type
 def test_thermo_scales():
+    s6 = status_code()
     t6 = deviceInfo("thermometer", "t5", "lbs")
     t6.set_measurements(200)
-    read_data("a1b2c3", t6, status)
+    read_data("a1b2c3", t6, s6.error)
     expected = ['Invalid measurements']
     # captured = capsys.readouterr()
     
-    assert expected == status.error
+    assert expected == s6.error
