212e3c04b859771cb344df5f3fac285ee305e2c5
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-02-17 00:55:36 -0500

    change input/output to json

--- devices.py
+++ devices.py
@@ -1,5 +1,7 @@
 # Device interface skeleton - Celsius and Farenheit
+import json
 from datetime import datetime
+
 now = datetime.now()
 
 keys = [
@@ -17,13 +19,22 @@ keys = [
 #             "oximeter"
 #             )
 
-devices = {
+valid_devices = {
             "thermometer": ("C", "F"), 
             "bp_monitor": ("mmHg", "bpm"), 
             "w_scale": ("kgs", "lbs"), 
             "glucose_meter": "mg/dL", 
             "oximeter": ("SP02", "bpm"),
             }
+
+# deviceInfo = {
+#                 "name": str, *
+#                 "type": str, *     Enum: [thermometer, bp_monitor, w_scale, glucose_meter, oximeter]
+#                 "id": int,
+#                 "measurement": [int], 
+#                 "unit": str, *
+#                 "time_stamp": str
+#               }
 
 
 class status_code:
@@ -33,31 +44,50 @@ class status_code:
 
 
 class deviceInfo:
-    # def __init__(self, type, name, scale) -> None:
-    def __init__(self, *args) -> None:  # type, name, scale (optional)
+
+    ''' def __init__(self, *args) -> None:  # type, name, unit (optional)
         self.type = args[0]
         self.name = args[1]
         self.serialNumber = None
         self.measurement = None
+    '''
 
-        # >> Should device check be here?
+    # >> Should device check be here?
 
+    '''        
         # Check for unit scale (for now is just thermometer C/F)
         if len(args) == 3 and isinstance(args[2], str):
             # if args[2] == "C" or args[2] == "F":
             self.unit = args[2]
         else:
             self.unit = None
+    '''
+
+    def __init__(self, name, type, serial_number, measurement, unit) -> None:
+        self.name = name
+        self.type = type
+        self.serial_number = serial_number
+        self.measurement = measurement
+        self.unit = unit
+
 
     # type = ""
     # name = ""
     # serialNumber = None
     # measurement = None
 
-    def set_measurements(self, measurement):
+    # Manual input
+
+
+    '''
+        def set_measurements(self, measurement):
         self.measurement = measurement
         self.measurementsTime = now.strftime("%d/%m/%Y %H:%M:%S")
-    
+    '''
+
+    def set_time(self):
+        self.time_stamp = now.strftime("%d/%m/%Y %H:%M:%S")
+
 
 def check_range(device):
     # Error codes:
@@ -76,26 +106,55 @@ def check_range(device):
     return True
 
 
-def read_data(key, device, status):
-    # check key (hardcoded for now)
+def read_data(key, in_device, status):
+
+    # Add JSON as input
+
+    print("input: ", type(in_device), in_device)
+
+    # Convert (deserialize) JSON to dictionary & convert to deviceInfo object:
+    if type(in_device) is str:
+        try:
+            with open(in_device, "r") as read_file:
+                data = json.load(read_file)     # dict type
+            # print(type(data)) 
+            print(data)
+            device = deviceInfo(**data)
+            device.set_time()
+            print(device.__dict__)
+
+        except FileNotFoundError as e:
+            status.success = False
+            status.error.append(e.strerror)
+            return 
+
+        # print(type(device))
+        # >>> check json validity
+
+    elif type(in_device) is not deviceInfo:
+        status.success = False
+        status.error.append("Invalid input")
+        return
+    
+
+    # check key (hardcoded list for now)
     print(">> key ", key, keys)
 
     if key not in keys:
         status.success = False
         status.error.append("Invalid key")
 
-    # check device type
-    # print(device.type)
-    valid_device = devices.get(device.type)    # returns device's units 
-
+    valid_device = valid_devices.get(device.type)    # returns device's units 
     # print(valid_device)
-    # if device.type not in devices:
+
+    # check device type
     if valid_device is None:
         status.success = False
         status.error.append("Invalid device type")
 
     # check correct units for device
     if valid_device is not None and device.unit not in valid_device:
+        print("in check")
         status.success = False
         status.error.append("Invalid units for device")
 
@@ -103,3 +162,22 @@ def read_data(key, device, status):
     if not check_range(device):
         status.success = False
         status.error.append("Invalid measurements")
+
+    # Return JSON with timestamp 
+    # Create (serialize) JSON from deviceInfo object:
+    out_json = json.dumps(device.__dict__, indent=4)
+    # print(encode)
+    # print(type(encode))
+    return out_json
+
+
+# t = deviceInfo("thermometer", "t1", "C")
+# print(t.type, t.name, t.unit)
+# t.set_measurements(111)
+# print(t.measurement)
+# print(t.measurementsTime)
+
+# status = status_code()
+# out = read_data("a1b2c3", "device1.json", status)
+# print(status.success, status.error)
+# print(out)
