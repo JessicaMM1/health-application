2827087b5f4fd9b5204350e41857d7528e5be0b4
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-03-03 04:56:17 -0500

    deploy to google cloud

--- api_test.py
+++ api_test.py
@@ -1,8 +1,8 @@
 # Test API - web app
 
-import requests
+# import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000/devices"
 
 put_device_info = {
                     "name": "t13343", 
@@ -13,6 +13,6 @@ put_device_info = {
                     }
 
 # response = requests.get(BASE + "a1b2c3/device1.json")
-response = requests.put(BASE + "a1b2c3/device9.json", put_device_info)
+# response = requests.put(BASE + "a1b2c3/device9.json", put_device_info)
 # print(type(response)) # requests.model.Response
-print(response.json())
+# print(response.json())
--- device_api.py
+++ device_api.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from devices import add_data, status_code, read_data
 
-app = Flask("DeviceAPI")
+app = Flask(__name__)
 api = Api(app)
 
 # Required arguments
@@ -38,7 +38,7 @@ class DeviceAPI(Resource):
             return status_put.error, 400     # bad request
 
 
-api.add_resource(DeviceAPI,"/<string:key>/<string:file>")
+api.add_resource(DeviceAPI,"/devices/<string:key>/<string:file>")
 
 if __name__ == '__main__':
     app.run(debug=True)
